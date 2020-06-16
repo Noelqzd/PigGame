a1d58da75610605a4909bb6797492779c50650d5
Noel Quezada <noel23quezada@gmail.com>
2020-06-16 12:44:19 -0700

    implement HOLD function

--- app.js
+++ app.js
@@ -23,18 +23,7 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
         roundScore += dice;
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
-
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-        
-        // document.querySelector('.player-0-panel').classList.remove('active');
-        // document.querySelector('.player-1-panel').classList.add('active');
-document.querySelector('dice').style.display = 'none';
+        nextPlayer();
 
     }
 
@@ -42,13 +31,37 @@ document.querySelector('dice').style.display = 'none';
 
 });
 
+document.querySelector('.btn-hold').addEventListener('click', function () {
+    scores[activePlayer] += roundScore;
+
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    if (scores[activePlayer] >= 20) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    } else {
+        nextPlayer();
+    }
+
+    nextPlayer();
+});
 
 
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
 
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
 
-// document.querySelector('#current-' + activePlayer).textContent = dice;
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
 
-// let x = document.querySelector('#score-0').textContent;
+    document.querySelector('dice').style.display = 'none';
+
+}
 
 
 
