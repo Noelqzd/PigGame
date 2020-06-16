2dea46098d829d5f646dbc00a46f62c4fea8a901
Noel Quezada <noel23quezada@gmail.com>
2020-06-16 12:00:21 -0700

    updating scores and change active player

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 
@@ -18,6 +18,27 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
     let diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
+
+    if (dice !== 1) {
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        
+        // document.querySelector('.player-0-panel').classList.remove('active');
+        // document.querySelector('.player-1-panel').classList.add('active');
+document.querySelector('dice').style.display = 'none';
+
+    }
+
+
 
 });
 
