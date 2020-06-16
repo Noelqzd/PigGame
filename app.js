b7c56fab8c5f3d094be939da00b4950d50d21203
Noel Quezada <noel23quezada@gmail.com>
2020-06-16 10:18:34 -0700

    event handling

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
-let scores, roundScore, activePlayer, dice;
+let scores, roundScore, activePlayer;
 
-scores = [0,0];
+scores = [0, 0];
 roundScore = 0;
-activePlayer = 0;
-
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-
-let x = document.querySelector('#score-0').textContent;
-console.log(x);
+activePlayer = 1;
 
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+
+    let dice = Math.floor(Math.random() * 6) + 1;
+
+    let diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+});
 
 
 
+
+
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+
+// let x = document.querySelector('#score-0').textContent;
 
 
 
